ccded42929dac7b008ba495b06cd8bc66abf384a
Jack Liu <liujack@amazon.com>
2020-05-12 22:18:03 -0400

    test change

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>
+            Some stuff changed
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
